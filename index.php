b4415ffa1582fc78a3b9ca043098a571fc13ccea
Sahin031 <jlmnd92@gmail.com>
2016-08-30 00:58:12 -0300

    Alteração no arquivo index.php

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <title> Meu Site </title>
+</head>
+<body>
+</body>
+</html>
